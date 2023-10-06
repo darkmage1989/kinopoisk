df3a31d0ebd25cdf144d404d6173e96fdbc0daa2
darkmage1989 <darkmage1989@gmail.com>
2023-10-06 15:30:05 +0300

    настройка интерфейсов

--- src/components/MoviePageList/MoviePageList.tsx
+++ src/components/MoviePageList/MoviePageList.tsx
@@ -8,8 +8,8 @@ interface MoviePageListProps {
  
 const MoviePageList = ({data}:MoviePageListProps) => {
     return ( <div className={styles.movie__box}>
-        <div className={styles.movie__text}>
-          <h2>Название: {data.name}</h2>
+        <div>
+          <h2 className={styles.movie__title}>Название: {data.name}</h2>
           <div className={cx("movie__text_row", "movie__text")}>
             <span>Cборы в мире: {data.fees.world.value} $</span>
             <span>Рейтинг: {data.rating.kp.toFixed(1)}</span>
@@ -27,9 +27,9 @@ const MoviePageList = ({data}:MoviePageListProps) => {
         <div className={styles.actors__block}>
           {data.persons.map((person: person) => (
             <div className={styles.actor__box} key={person.id}>
-              <span>Актер: {person.name}</span>
+              <span style={{textAlign:'center'}}>{person.name}</span>
               <img className={styles.actor__img} src={person.photo} alt={person.photo} />
-              <span>Роль: {person.description}</span>
+              <span style={{textAlign:'center'}}>{person.description}</span>
             </div>
           ))}
         </div>
